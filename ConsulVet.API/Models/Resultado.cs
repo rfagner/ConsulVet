30e217af2b79c305b82dea1020c8f3996a87845b
Fagner Santos <uprogramador@gmail.com>
2022-09-02 10:07:52 -0300

    Foi implementado um objeto para carregar os dados do Cliente/Veterinário nas 'Consultas' e 'Resultados'

--- ConsulVet.API/Models/Resultado.cs
+++ ConsulVet.API/Models/Resultado.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using ConsulVet.API.Repositories;
+using System.ComponentModel.DataAnnotations;
 
 namespace ConsulVet.API.Models
 {
@@ -8,13 +9,48 @@ namespace ConsulVet.API.Models
 
         [Required(ErrorMessage = "Informe o resultado do diagnóstico")]
         public string Diagnostico { get; set; }
+        private int _clienteId;
 
         [Required(ErrorMessage = "Informe o ID do Cliente")]
-        public int ClienteId { get; set; }
+        public int ClienteId
+        {
+            get
+            {
+                return _clienteId;
+            }
+            set
+            {
+                // Carrega o objeto com os dados do Cliente
+                _clienteId = value;
+                ClienteRepository clienteRepository = new ClienteRepository();
+                if (_clienteId != 0)
+                {
+                    Cliente = clienteRepository.GetById(_clienteId);
+                }
+
+            }
+        }
         public virtual Cliente Cliente { get; set; }
+        private int _veterinarioId;
 
         [Required(ErrorMessage = "Informe o ID do Veterinário")]
-        public int VeterinarioId { get; set; }
+        public int VeterinarioId
+        {
+            get
+            {
+                return _veterinarioId;
+            }
+            set
+            {
+                // Carrega o objeto com os dados do Veterinário
+                _veterinarioId = value;
+                VeterinarioRepository veterinarioRepository = new VeterinarioRepository();
+                if (_veterinarioId != 0)
+                {
+                    Veterinario = veterinarioRepository.GetById(_veterinarioId);
+                }
+            }
+        }
         public virtual Veterinario Veterinario { get; set; }
     }
 }
